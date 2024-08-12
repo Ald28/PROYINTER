b40cb67059a06e41c9b1cd678a777ae3c1708239
Jennyfer Sapallanay <jenifersasa2004@gmail.com>
2024-08-12 12:17:09 -0500

    Mejore la responsividad de la vista de Despacho, modifique los archivos Despacho.jsx y Despacho.css

--- src/pages/services/despacho/Despacho.jsx
+++ src/pages/services/despacho/Despacho.jsx
@@ -3,6 +3,7 @@ import { useNavigate, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFileCircleCheck, faListUl } from '@fortawesome/free-solid-svg-icons';
 import '../../../assets/styles/Despacho.css';
+import { Container } from 'react-bootstrap'; // Importamos el componente Container de Bootstrap
 
 const Despacho = () => {
     const navigate = useNavigate();
@@ -28,19 +29,19 @@ const Despacho = () => {
                 </ol>
             </nav>
             <h2>Despacho</h2>
-            <div className="button-container">
-                <button className="despacho-button fw-bold" onClick={() => handleLinkClick('/cliente/despacho/guia-remision')}>
+            <Container className="d-flex flex-column align-items-center mx-auto mt-4" style={{ maxWidth: '360px' }}>
+                <button className="despacho-button fw-bold mb-3 w-100" onClick={() => handleLinkClick('/cliente/despacho/guia-remision')}>
                     <FontAwesomeIcon icon={faFileCircleCheck} className="button-icon" />
                     <span>Guía de Remisión</span>
                 </button>
-                
-                <button className="despacho-button" onClick={() => handleLinkClick('/cliente/despacho/packing-list')}>
+                <button className="despacho-button mb-3 w-100" onClick={() => handleLinkClick('/cliente/despacho/packing-list')}>
                     <FontAwesomeIcon icon={faListUl} className="button-icon" />
                     <span>Packing List</span>
                 </button>
-            </div>
+            </Container>
         </div>
     );
 };
 
 export default Despacho;
+
