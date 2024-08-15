45d2967c367c0548e1ccc3bb91ef76306153f49c
Ald28 <aldopichihua123@gmail.com>
2024-08-15 14:28:58 -0500

    garantias

--- src/pages/reclamos/Garantias.jsx
+++ src/pages/reclamos/Garantias.jsx
@@ -1,8 +1,125 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import 'datatables.net-dt/css/dataTables.dataTables.css';
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { faFileAlt } from '@fortawesome/free-solid-svg-icons';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Ensure Bootstrap JS is included
+
 export default function Garantias() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const [showWarning, setShowWarning] = useState(false); // State for modal
+
+    const orders = [
+        {
+            id: 1,
+            title: 'Fabricacion de Tanques de Fibre',
+            fecha: '2025-11-07'
+        },
+        {
+            id: 2,
+            title: 'Suministro de bomba dosificadora',
+            fecha: '2024-06-29'
+        }
+    ];
+
+    useEffect(() => {
+        const table = $('#orderTable').DataTable({
+            paging: false,
+            searching: false,
+            info: false
+        });
+
+        return () => {
+            // Destruye la instancia de DataTable cuando el componente se desmonte
+            if ($.fn.dataTable.isDataTable('#orderTable')) {
+                table.destroy();
+            }
+        };
+    }, []);
+
+    const navigate = useNavigate();
+    const handleLinkClick = (path) => {
+        navigate(path);
+    };
+
+    const handleRequestGuarantee = (id) => {
+        if (id === 2) {
+            setShowWarning(true); // Show warning modal for ID 2
+        }
+    };
+
+    return (
+        <div>
+            <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item">
+                        <button className="btn btn-black fw-bold p-0" onClick={() => handleLinkClick('/cliente')}>Cliente</button>
+                    </li>
+                    <li className="breadcrumb-item">
+                        <button className="btn btn-black fw-bold p-0" onClick={() => handleLinkClick('/cliente/garantias-reclamos')}>Garantias y Reclamos</button>
+                    </li>
+                    <li className="breadcrumb-item activate" aria-current="page">Garantias</li>
+                </ol>
+            </nav>
+
+            <h4>Servicios de procura adquiridos</h4>
+            <table id="orderTable" className="display">
+                <thead>
+                    <tr>
+                        <th>N°</th>
+                        <th>Servicio de Procura</th>
+                        <th>Vigencia de Garantía</th>
+                        <th>Acción</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {orders.map(order => (
+                        <tr key={order.id}>
+                            <td>
+                                <div className="info-section">
+                                    <div className="info-item">
+                                        {order.id}
+                                    </div>
+                                </div>
+                            </td>
+                            <td>{order.title}</td>
+                            <td>{order.fecha}</td>
+                            <td>
+                                <Link className="custom-button fw-normal" onClick={() => handleRequestGuarantee(order.id)} to="#">
+                                    <i className="fas fa-file-alt"></i>Solicitar Garantía
+                                </Link>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            <div
+                className={`modal fade ${showWarning ? 'show d-block' : ''}`} tabIndex="-1"
+                style={showWarning ? { display: 'block', backgroundColor: 'rgba(0,0,0,0.5)' } : { display: 'none' }} role="dialog">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Servicio sin Garantia Vigente</h5>
+                            <button type="button" className="btn-close" onClick={() => setShowWarning(false)} aria-label="Close" />
+                        </div>
+                        <div className="modal-body">
+                            <p>Su servicio de Procura de Suministro de Bomba Dosificadora no cuenta con garantía vigente. Para obtener más información o solicitar una oferta de repuestos o reposición del equipo, haga clic en el siguiente botón.</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-success">
+                                Solicitar Oferta
+                            </button>
+                            <button type="button" className="btn btn-warning" onClick={() => setShowWarning(false)}>
+                                Cerrar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
 }
--- src/pages/reclamos/Garantias_Reclamos.jsx
+++ src/pages/reclamos/Garantias_Reclamos.jsx
@@ -6,31 +6,31 @@ import { faFileShield, faPersonCircleExclamation } from '@fortawesome/free-solid
 import { Container } from 'react-bootstrap';
 
 export default function Reclamos() {
-  const navigate = useNavigate();
-  const handleLinkClick = (path) => {
-    navigate(path);
-  };
-  return (
-    <div>
-      <nav aria-label="breadcrumb">
-        <ol className="breadcrumb">
-          <li className="breadcrumb-item">
-            <button className="btn btn-black fw-bold p-0" onClick={() => handleLinkClick('/cliente')}>Cliente</button>
-          </li>
-          <li className="breadcrumb-item activate" aria-current="page">Garantias y Reclamos</li>
-        </ol>
-      </nav>
-      <h2>Garantías y Reclamos </h2>
-      <Container className="d-flex flex-column align-items-center mx-auto mt-4" style={{ maxWidth: '360px' }}>
-        <button className="despacho-button fw-bold mb-3 w-100" onClick={() => handleLinkClick('/cliente/garantias-reclamos/garantias')}>
-          <FontAwesomeIcon icon={faFileShield} className='button-icon' />
-          <span>Garantías</span>
-        </button>
-        <button className="despacho-button mb-3 w-100" onClick={() => handleLinkClick('/cliente/garantias-reclamos/reclamos')}>
-          <FontAwesomeIcon icon={faPersonCircleExclamation} className='button-icon' />
-          <span>Reclamos</span>
-        </button>
-      </Container>
-    </div>
-  )
+    const navigate = useNavigate();
+    const handleLinkClick = (path) => {
+        navigate(path);
+    };
+    return (
+        <div>
+            <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item">
+                        <button className="btn btn-black fw-bold p-0" onClick={() => handleLinkClick('/cliente')}>Cliente</button>
+                    </li>
+                    <li className="breadcrumb-item activate" aria-current="page">Garantias y Reclamos</li>
+                </ol>
+            </nav>
+            <h2>Garantías y Reclamos </h2>
+            <Container className="d-flex flex-column align-items-center mx-auto mt-4" style={{ maxWidth: '360px' }}>
+                <button className="despacho-button fw-bold mb-3 w-100" onClick={() => handleLinkClick('/cliente/garantias-reclamos/garantias')}>
+                    <FontAwesomeIcon icon={faFileShield} className='button-icon' />
+                    <span>Garantías</span>
+                </button>
+                <button className="despacho-button mb-3 w-100" onClick={() => handleLinkClick('/cliente/garantias-reclamos/reclamos')}>
+                    <FontAwesomeIcon icon={faPersonCircleExclamation} className='button-icon' />
+                    <span>Reclamos</span>
+                </button>
+            </Container>
+        </div>
+    )
 }
