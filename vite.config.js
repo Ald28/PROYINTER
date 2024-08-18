48ed7b3536b55f6a73a48b40b780e7a8f5c318e1
Ald28 <aldopichihua123@gmail.com>
2024-08-18 02:34:00 -0500

    arreglo del laravel con react

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,4 @@
 import { defineConfig } from 'vite';
-<<<<<<< HEAD
-import laravel from 'laravel-vite-plugin';
-
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-=======
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
@@ -19,5 +8,4 @@ export default defineConfig({
       '@': '/src',
     },
   },
->>>>>>> cb570893ad3d42f1d6dd9138fe214b75b61bc23d
 });
